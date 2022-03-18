256d0848e8cc5e38ff7d2b834417bbfb006d8747
Jhenzel201880208 <92150970+Jhenzel201880208@users.noreply.github.com>
2022-03-18 20:21:32 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '087c4a9f020346c7856eb0b2a0d05a844f17503d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '087c4a9f020346c7856eb0b2a0d05a844f17503d',
+            'dev_requirement' => false,
+        ),
+        'twig/twig' => array(
+            'pretty_version' => 'v1.13.2',
+            'version' => '1.13.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twig/twig',
+            'aliases' => array(),
+            'reference' => '6d6a1009427d1f398c9d40904147bf9f723d5755',
+            'dev_requirement' => false,
+        ),
+    ),
+);
